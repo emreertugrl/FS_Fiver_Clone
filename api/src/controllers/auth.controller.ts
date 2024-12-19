b417894d6ce795792c0a1ed9676683e0695e042e
emreertugrl <emrertugrl@gmail.com>
2024-12-19 16:18:25 +0300

    register response ready

--- api/src/controllers/auth.controller.ts
+++ api/src/controllers/auth.controller.ts
@@ -1,9 +1,21 @@
 import { NextFunction, Request, Response } from "express";
+import bcrypt from "bcrypt";
+import User from "../models/user.model";
 
 export const register = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-  console.log(req);
+  // şifreyi hashleme
+  const hashedPass = bcrypt.hashSync(req.body.password, 12);
 
-  res.status(200).json({ message: "İşlem Başarılı" });
+  // Kullanıcıyı veritabanına kaydet
+  const newUser = await User.create({
+    ...req.body,
+    password: hashedPass,
+  });
+
+  // password'i undefined olarak sıfırla (gizle)
+  newUser.password = undefined;
+
+  res.status(200).json({ message: "Hesabınız oluşturuldu", data: newUser });
 };
 
 export const login = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
