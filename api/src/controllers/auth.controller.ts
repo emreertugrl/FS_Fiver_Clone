0614183f4dfee9d18859db6c72fba11fb987ffea
emreertugrl <emrertugrl@gmail.com>
2024-12-19 16:00:57 +0300

    bcryp download

--- api/src/controllers/auth.controller.ts
+++ api/src/controllers/auth.controller.ts
@@ -1,6 +1,8 @@
 import { NextFunction, Request, Response } from "express";
 
 export const register = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+  console.log(req);
+
   res.status(200).json({ message: "İşlem Başarılı" });
 };
 
