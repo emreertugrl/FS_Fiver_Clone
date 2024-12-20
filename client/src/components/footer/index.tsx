5e17304d9668d7cdd7226ba6160fbbe908cf2096
emreertugrl <emrertugrl@gmail.com>
2024-12-20 18:19:45 +0300

    header and footer fixed

--- client/src/components/footer/index.tsx
+++ client/src/components/footer/index.tsx
@@ -1,5 +1,7 @@
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="w-full max p-5 border-t text-center">Tüm Hakları Saklıdır &copy;</footer>
+  );
 };
 
 export default Footer;
--- client/src/components/header/index.tsx
+++ client/src/components/header/index.tsx
@@ -7,7 +7,7 @@ const Header = () => {
   const user = true;
   return (
     <header className="p-5 shadow">
-      <div className="max-w-[1440px] mx-auto flex justify-between gap-4 md:gap-8">
+      <div className="max flex justify-between gap-4 md:gap-8">
         <Link to="/">
           <img src="/fiverr.png" alt="fiverr logo" className="w-[100px]" />
         </Link>
