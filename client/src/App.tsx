8a36e88e0fff51a1ed7f95427b2929ecdbab651c
emreertugrl <emrertugrl@gmail.com>
2024-12-20 16:15:38 +0300

    files opened

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,22 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "./components/header";
+import Register from "./pages/register";
+import Login from "./pages/login";
+import Home from "./pages/home";
+
 const App = () => {
-  return <div className="">App</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
