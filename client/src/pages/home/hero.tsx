4e2a541e3aae03309a2da0c3dfd85594288d7e95
emreertugrl <emrertugrl@gmail.com>
2024-12-20 20:21:46 +0300

    form defined to hero

--- client/src/pages/home/hero.tsx
+++ client/src/pages/home/hero.tsx
@@ -1,5 +1,15 @@
+import { FormEvent } from "react";
 import { IoSearch } from "react-icons/io5";
+import { useNavigate } from "react-router-dom";
 const Hero = () => {
+  const navigate = useNavigate();
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    // inputa yazılan değere erişme
+    const text = (e.currentTarget[0] as HTMLInputElement).value;
+
+    navigate(`/search?query=${text}`);
+  };
   return (
     <div>
       <section className="bg-f-green max-md:-m-5 h-[40vh] text-white px-6 py-5 md:px-12 md:py-10 md:rounded-md flex flex-col justify-center items-center">
@@ -8,7 +18,7 @@ const Hero = () => {
             Profesyonel iş gücünüzü <span className="font-serif">freelancerlarla</span>{" "}
             ölçeklendirin
           </h1>
-          <form className="bg-white rounded-md w-full flex gap-5 mt-10">
+          <form onSubmit={handleSubmit} className="bg-white rounded-md w-full flex gap-5 mt-10">
             <input
               type="text"
               className="flex-1 p-2  rounded-md text-black outline-none"
