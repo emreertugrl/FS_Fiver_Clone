e86824bfd383591e2cc6c6a5ebcce6747bcff4fd
emreertugrl <emrertugrl@gmail.com>
2024-12-23 19:01:35 +0300

    gig routes defined to project

--- api/src/controllers/gig.controller.ts
+++ api/src/controllers/gig.controller.ts
@@ -1,9 +1,24 @@
 import { NextFunction, Request, Response } from "express";
+import c from "../utils/catchAsync.ts";
 
-export const getAllGigs = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-): Promise<void> => {
+export const getAllGigs = c(
+  async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    res.status(200).json({ message: "İşlem Başarılı" });
+  }
+);
+
+export const getGig = c(async (req: Request, res: Response, next: NextFunction): Promise<void> => {
   res.status(200).json({ message: "İşlem Başarılı" });
-};
+});
+
+export const createGig = c(
+  async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    res.status(200).json({ message: "İşlem Başarılı" });
+  }
+);
+
+export const deleteGig = c(
+  async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    res.status(200).json({ message: "İşlem Başarılı" });
+  }
+);
--- api/src/routes/gig.routes.ts
+++ api/src/routes/gig.routes.ts
@@ -1,9 +1,10 @@
 import express, { Router } from "express";
+import { createGig, deleteGig, getAllGigs, getGig } from "../controllers/gig.controller.ts";
 // 1) router oluşturma
 const router: Router = express.Router();
 // 2) yollar belirlenir.
-router.route("/").get();
-router.route("/:id").get();
+router.route("/").get(getAllGigs).post(createGig);
+router.route("/:id").get(getGig).delete(deleteGig);
 
 // 3) router'i app'e tanıtmak için export ederiz.
 
