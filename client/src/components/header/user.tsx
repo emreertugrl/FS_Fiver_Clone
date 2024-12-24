fa9e186401e5b4cdbe9f5cbe758786eac8fefae2
emreertugrl <emrertugrl@gmail.com>
2024-12-24 15:07:46 +0300

    header fixed

--- client/src/components/header/user.tsx
+++ client/src/components/header/user.tsx
@@ -10,12 +10,16 @@ const User = ({ data, logout }: Props) => {
       <img src={data.photo} className="size-10 rounded-full object-cover" />
       <span>{data.username}</span>
       <div className="w-[150px] text-sm flex-col hidden group-hover:flex text-center absolute top-10 left-0 transition duration-500 bg-gray-200 rounded-md ">
-        <Link to="/my-gigs" className="px-5 py-2 hover:bg-gray-100 hover:rounded-t-md">
-          Hizmetler
-        </Link>
-        <Link to="/add-gigs" className="px-5 py-2 hover:bg-gray-100 text-nowrap">
-          Hizmet Ekle
-        </Link>
+        {data.isSeller && (
+          <>
+            <Link to="/my-gigs" className="px-5 py-2 hover:bg-gray-100 hover:rounded-t-md">
+              Hizmetler
+            </Link>
+            <Link to="/add-gigs" className="px-5 py-2 hover:bg-gray-100 text-nowrap">
+              Hizmet Ekle
+            </Link>
+          </>
+        )}
         <Link to="/" className="px-5 py-2 hover:bg-gray-100 text-nowrap">
           Siparişler
         </Link>
